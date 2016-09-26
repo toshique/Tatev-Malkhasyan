a4cb317c03f44ccf723be2fed9cc4a34991f9787
toshique <tatevik_malkhasyan@edu.aua.am>
2016-09-26 18:36:16 +0400

    Add files via upload

--- triangleStars.js
+++ triangleStars.js
@@ -1,36 +1,31 @@
 
 <script>
+
 const stars=function(n){
 			if(n <= 0){
-				return '';
+				return'';
 			}
 			return '*' + stars(n-1);
 		}
-		const spaces=function(n){
+const spaces=function(n){
 			if(n <= 0){
-				return '';
+				return'';
 			}
 			return ' ' + spaces(n-1);
 		}
-		const run=function(n){
-			if (n <= 0) {
-				return;
-			}
-			run(n-1);
-		}
 
 			
-		const run2=function(SpaceCount, StarCount, n){
+const run=function(SpaceCount, StarCount, n){
 			if (n===0){
 				return;
 			}
 			console.log(spaces(SpaceCount)+stars(StarCount));
-			run2(SpaceCount-1, StarCount+2, n-1);
-			run(n-1,1,n);
+			run(SpaceCount-1, StarCount+2, n-1);
+			
 		}
-		const run1 = function(n) {
-			run2( (n + n - 2)/2, 1, n);
+const triangleStars = function(n) {
+			run(n-1, 1, n);
 		}
 
-		run1(5);
+		triangleStars(7);
 </script>
